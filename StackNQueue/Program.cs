47a0a7ffb1b3ec25101d7cfd0531d934a7847cdb
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-21 23:13:09 +0530

    [Sai]First commit

---
+++ StackNQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackNQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack and Queue Operations!");
+        }
+    }
+}

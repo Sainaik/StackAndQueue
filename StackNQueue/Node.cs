de4d9ddb713c8d177cb81f626296d610183b3920
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-21 23:19:08 +0530

    [Sai]Added. Elements can be pushed and displayed

---
+++ StackNQueue/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackNQueue
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node()
+        {
+
+        }
+
+        public Node(int data)
+        {
+            this.data = data;
+
+        }
+    }
+}
--- StackNQueue/Program.cs
+++ StackNQueue/Program.cs
@@ -7,6 +7,13 @@ namespace StackNQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Stack and Queue Operations!");
+
+            Stack stack = new Stack();
+
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.Display();
         }
     }
 }
